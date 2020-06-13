464e972a84e011236f51e0bc3a2ad7ff402e377f
Brent Whitehead <brent.whitehead@tcnbroadcasting.com>
2020-06-12 22:16:08 -0600

    revamped to canvas editor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from "react"
-import RandomColor from "./randomColor"
-import RandomColor2 from "./RandomColor2"
-import RandomColor3 from "./RandomColor3"
+import React, { useRef, useEffect, useState } from "react"
 import "./App.css"
-import { useSetRCValues } from "./context"
 
 function App() {
-  console.log("app rendered")
-  const setRC = useSetRCValues()
+  const canvas = useRef()
+  const position = useRef(10)
 
-  return (
-    <div className='App'>
-      <RandomColor />
-      <RandomColor2 />
-      <RandomColor3 />
-      <button style={{ position: "absolute", top: 0 }} onClick={() => setRC(Math.random())}>
-        click me
-      </button>
-    </div>
-  )
+  const handleMove = (e) => {
+    const position = {
+      x: 10,
+      y: 10
+    }
+    const handleRight = () => {
+      console.log("handling right")
+      position.x += 1
+    }
+    const handleUp = () => postion.y += 1
+    const handleLeft = () => position.x -= 1
+    const handleDown = () => position.y -= 1
+
+    switch (e.keyCode) {
+      case 37:
+        handleLeft()
+        break
+      case 38:
+        handleUp()
+        break
+      case 39:
+        handleRight()
+        break
+      case 40:
+        handleDown()
+        break
+      default:
+        return
+    }
+
+    return position
+  }
+
+  const renderCharacter = () => {
+    const ctx = canvas.current.getContext("2d")
+    ctx.fillRect(, posY, 50, 50)
+  }
+
+  useEffect(() => {
+    renderCharacter()
+    document.addEventListener("keydown", handleMove)
+  }, [])
+
+  return <canvas ref={canvas} width={500} height={500} tabIndex={1} />
 }
 
 export default App
