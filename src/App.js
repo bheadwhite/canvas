7fc83916c5f1447abcac68165cd4b97ac35f2ae6
Brent Whitehead <brent.whitehead@tcnbroadcasting.com>
2020-06-13 22:42:50 -0600

    canvas v1

--- src/App.js
+++ src/App.js
@@ -1,54 +1,124 @@
-import React, { useRef, useEffect, useState } from "react"
+import React, { useRef, useEffect, useCallback } from "react"
 import "./App.css"
 
 function App() {
   const canvas = useRef()
-  const position = useRef(10)
-
-  const handleMove = (e) => {
-    const position = {
-      x: 10,
-      y: 10
-    }
-    const handleRight = () => {
-      console.log("handling right")
-      position.x += 1
-    }
-    const handleUp = () => postion.y += 1
-    const handleLeft = () => position.x -= 1
-    const handleDown = () => position.y -= 1
-
-    switch (e.keyCode) {
-      case 37:
-        handleLeft()
-        break
-      case 38:
-        handleUp()
-        break
-      case 39:
-        handleRight()
-        break
-      case 40:
-        handleDown()
-        break
-      default:
-        return
-    }
-
-    return position
-  }
-
-  const renderCharacter = () => {
+  const position = useRef({ x: 10, y: 10 })
+  const keys = []
+  const clear = () => {
     const ctx = canvas.current.getContext("2d")
-    ctx.fillRect(, posY, 50, 50)
+    ctx.clearRect(position.current.x, position.current.y, 50, 50)
+  }
+  const draw = () => {
+    const ctx = canvas.current.getContext("2d")
+    ctx.fillRect(position.current.x, position.current.y, 50, 50)
   }
 
-  useEffect(() => {
-    renderCharacter()
-    document.addEventListener("keydown", handleMove)
+  const handleLeft = useCallback(() => {
+    console.log("left")
+    clear()
+    position.current.x -= 5
+    draw()
+  }, [])
+  const handleRight = useCallback(() => {
+    console.log("right")
+    clear()
+    position.current.x += 5
+    draw()
+  }, [])
+  const handleUp = useCallback(() => {
+    console.log("up")
+    clear()
+    position.current.y -= 5
+    draw()
+  }, [])
+  const handleDown = useCallback(() => {
+    console.log("down")
+    clear()
+    position.current.y += 5
+    draw()
+  }, [])
+  const handleUpLeft = useCallback(() => {
+    clear()
+    position.current.y -= 5
+    position.current.x -= 5
+    draw()
+  }, [])
+  const handleUpRight = useCallback(() => {
+    clear()
+    position.current.y -= 5
+    position.current.x += 5
+    draw()
+  }, [])
+  const handleDownLeft = useCallback(() => {
+    clear()
+    position.current.y += 5
+    position.current.x -= 5
+    draw()
+  }, [])
+  const handleDownRight = useCallback(() => {
+    clear()
+    position.current.y += 5
+    position.current.x += 5
+    draw()
   }, [])
 
-  return <canvas ref={canvas} width={500} height={500} tabIndex={1} />
+  useEffect(() => {
+    const includesKeys = (a, b) => keys.indexOf(a) !== -1 && keys.indexOf(b) !== -1
+    draw()
+    document.addEventListener("keydown", (e) => {
+      console.log("keydown")
+      if (keys.indexOf(e.keyCode) === -1) {
+        keys.push(e.keyCode)
+      }
+      if (keys.length > 1) {
+        if (includesKeys(38, 37)) {
+          return handleUpLeft()
+        } else if (includesKeys(38, 39)) {
+          return handleUpRight()
+        } else if (includesKeys(40, 39)) {
+          return handleDownRight()
+        } else if (includesKeys(40, 37)) {
+          return handleDownLeft()
+        }
+      } else {
+        switch (e.keyCode) {
+          case 37:
+            handleLeft()
+            break
+          case 38:
+            handleUp()
+            break
+          case 39:
+            handleRight()
+            break
+          case 40:
+            handleDown()
+            break
+          default:
+            return
+        }
+      }
+    })
+    document.addEventListener("keyup", (e) => {
+      if (keys.indexOf(e.keyCode) !== -1) {
+        const index = keys.indexOf(e.keyCode)
+        keys.splice(index, 1)
+      }
+    })
+  }, [
+    handleDown,
+    handleLeft,
+    handleUp,
+    handleRight,
+    handleDownLeft,
+    handleDownRight,
+    handleUpLeft,
+    handleUpRight,
+    keys,
+  ])
+
+  return <canvas ref={canvas} width={1000} height={1000} tabIndex={1} />
 }
 
 export default App
